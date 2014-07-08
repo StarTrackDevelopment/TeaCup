0370da3ac1e3cb40fb5100f22c44fb98c9aef996
askis <akos.kis33@gmail.com>
2014-07-08 15:52:02 +0200

    Reverting to the state 9ec325e1cb

---
+++ packages/teacups/server/controllers/teacups.js
@@ -0,0 +1,124 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Teacup = mongoose.model('Teacup'),
+    _ = require('lodash');
+
+
+var getParameterValue = function(name, query) {
+    if (!query)
+        return null;
+    var pairs = query.split('&amp;');
+    var numberOfArguments = pairs.length;
+    for (var i = 0; i < numberOfArguments; i++) {
+        var pos = pairs[i].indexOf('=');
+
+        if (pos === -1) {
+            continue;
+        }
+
+        var argname = pairs[i].substring(0, pos);
+        var value = pairs[i].substring(pos + 1);
+
+        if (argname === name)
+            return value;
+    }
+    return null;
+};
+/**
+ * Find teacup by id
+ */
+exports.teacup = function(req, res, next, id) {
+    Teacup.load(id, getParameterValue('populate', req._parsedUrl.query), function (err, teacup) {
+        if (err) return next(err);
+        if (!teacup) return next(new Error('Failed to load teacup ' + id));
+        req.teacup = teacup;
+        next();
+    });
+};
+
+/**
+ * Create an teacup
+ */
+exports.create = function(req, res) {
+    var teacup = new Teacup(req.body);
+    teacup.user = req.user;
+
+    teacup.save(function(err) {
+        if (err) {
+            return res.send('users/signup', {
+                errors: err.errors,
+                teacup: teacup
+            });
+        } else {
+            res.jsonp(teacup);
+        }
+    });
+};
+
+/**
+ * Update an teacup
+ */
+exports.update = function(req, res) {
+    var teacup = req.teacup;
+
+    teacup = _.extend(teacup, req.body);
+
+    teacup.save(function(err) {
+        if (err) {
+            return res.send('users/signup', {
+                errors: err.errors,
+                teacup: teacup
+            });
+        } else {
+            res.jsonp(teacup);
+        }
+    });
+};
+
+/**
+ * Delete an teacup
+ */
+exports.destroy = function(req, res) {
+    var teacup = req.teacup;
+
+    teacup.remove(function(err) {
+        if (err) {
+            return res.send('users/signup', {
+                errors: err.errors,
+                teacup: teacup
+            });
+        } else {
+            res.jsonp(teacup);
+        }
+    });
+};
+
+/**
+ * Show an teacup
+ */
+exports.show = function(req, res) {
+    res.jsonp(req.teacup);
+};
+
+/**
+ * List of Articles
+ */
+exports.all = function(req, res) {
+    Teacup.find().sort('-created')
+    .populate('user speaker')
+    .populate('subscribedusers')
+    .populate('comments.createdby')
+    .exec(function (err, teacups) {
+        if (err) {
+            res.render('error', {
+                status: 500
+            });
+        } else {
+            res.jsonp(teacups);
+        }
+    });
+};
---
+++ packages/teacups/server/models/teacup.js
@@ -0,0 +1,86 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+/**
+ * Teacup Schema
+ */
+var TeacupSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    title: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    scheduleDate: {
+        type: Date,
+        default: Date.now
+        },
+    description: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    speaker: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    subscribedusers: [{
+        type: Schema.ObjectId,
+        ref: 'User'        
+    }],
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    },
+    comments: [{
+        createdwhen: {
+            type: Date,
+            default: Date.now
+        },
+        createdby: {
+            type: Schema.ObjectId,
+            ref: 'User'
+        },
+        comment: {
+            type: String,
+            default: '',
+            trim: true
+        }
+    }]
+});
+
+/**
+ * Validations
+ */
+TeacupSchema.path('title').validate(function(title) {
+    return title.length;
+}, 'Title cannot be blank');
+
+/**
+ * Statics
+ */
+TeacupSchema.statics.load = function (id, populate, cb) {
+    var populateobjects = (populate !== null && populate === 'true' ? 'user speaker' : 'user');
+    this.findOne({
+        _id: id
+    })
+    .populate(populateobjects)
+    .populate('subscribedusers')
+    .populate('comments.createdby')
+    .exec(cb);
+};
+
+mongoose.model('Teacup', TeacupSchema);
