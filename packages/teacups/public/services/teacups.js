58df374471fb8ee935dba550a387406093cdb76e
askis <akos.kis33@gmail.com>
2014-07-29 09:56:49 +0200

    pagination for Comments
    pagination for Comments

--- packages/teacups/public/services/teacups.js
+++ packages/teacups/public/services/teacups.js
@@ -159,3 +159,112 @@ angular.module('mean.teacups')
         template: '<a data-ng-href="/#!/users/{{userid}}"><i class="glyphicon glyphicon-user customgrey"></i></a>'
     };
 });
+
+angular.module('mean.teacups')
+
+    .filter('paginate', function (Paginator) {
+        return function (input, rowsPerPage) {
+            if (!input) {
+                return input;
+            }
+
+            if (rowsPerPage) {
+                Paginator.rowsPerPage = rowsPerPage;
+            }
+
+            Paginator.itemCount = input.length;
+
+            return input.slice(parseInt(Paginator.page * Paginator.rowsPerPage), parseInt((Paginator.page + 1) * Paginator.rowsPerPage + 1) - 1);
+        };
+    })
+
+    .filter('forLoop', function () {
+        return function (input, start, end) {
+            input = new Array(end - start);
+            for (var i = 0; start < end; start++, i++) {
+                input[i] = start;
+            }
+
+            return input;
+        };
+    })
+
+    .service('Paginator', function ($rootScope) {
+        this.page = 0;
+        this.rowsPerPage = 6;
+        this.itemCount = 0;
+        this.limitPerPage = 3;
+
+        this.setPage = function (page) {
+            if (page > this.pageCount()) {
+                return;
+            }
+
+            this.page = page;
+        };
+
+        this.nextPage = function () {
+            if (this.isLastPage()) {
+                return;
+            }
+
+            this.page++;
+        };
+
+        this.perviousPage = function () {
+            if (this.isFirstPage()) {
+                return;
+            }
+
+            this.page--;
+        };
+
+        this.firstPage = function () {
+            this.page = 0;
+        };
+
+        this.lastPage = function () {
+            this.page = this.pageCount() - 1;
+        };
+
+        this.isFirstPage = function () {
+            return this.page === 0;
+        };
+
+        this.isLastPage = function () {
+            return this.page === this.pageCount() - 1;
+        };
+
+        this.pageCount = function () {
+            return Math.ceil(parseInt(this.itemCount) / parseInt(this.rowsPerPage));
+        };
+
+        this.lowerLimit = function () {
+            var low = this.page;
+            low -= Math.ceil(this.limitPerPage/2);
+            low -= 1;
+            if (low < 0) {
+                return 0;
+            }
+            var pagecountminuslimit = this.pageCount();
+            pagecountminuslimit -= this.limitPerPage;
+            if (pagecountminuslimit < 0) {
+                return 0;
+            }
+            pagecountminuslimit += 1;
+            if (this.page > pagecountminuslimit ) {
+                return --pagecountminuslimit;
+            }
+            return low;
+        };
+    })
+
+    .directive('paginator', function factory() {
+        return {
+            restrict: 'E',
+            controller: function ($scope, Paginator) {
+                $scope.paginator = Paginator;
+            },
+            templateUrl: '/teacups/views/Directives/pagination.html'
+        };
+    });
