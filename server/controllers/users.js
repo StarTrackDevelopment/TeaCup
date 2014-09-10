4e3b2c664639dba55402c6bd17994c2c239e7a81
saslanidis <stefanos.aslanidis@googlemail.com>
2014-09-10 16:56:45 +0200

    added email check, trying to call nodemailer

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,20 +4,21 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    User = mongoose.model('User');
+    User = mongoose.model('User'),
+    nodemailer = require('nodemailer');
 
 /**
  * Auth callback
  */
-exports.authCallback = function(req, res) {
+exports.authCallback = function (req, res) {
     res.redirect('/');
 };
 
 /**
  * Show login form
  */
-exports.signin = function(req, res) {
-    if(req.isAuthenticated()) {
+exports.signin = function (req, res) {
+    if (req.isAuthenticated()) {
         return res.redirect('/');
     }
     res.redirect('#!/login');
@@ -26,7 +27,7 @@ exports.signin = function(req, res) {
 /**
  * Logout
  */
-exports.signout = function(req, res) {
+exports.signout = function (req, res) {
     req.logout();
     res.redirect('/');
 };
@@ -34,14 +35,48 @@ exports.signout = function(req, res) {
 /**
  * Session
  */
-exports.session = function(req, res) {
+exports.session = function (req, res) {
     res.redirect('/');
 };
+
+var sendMail = function (emailAddress) {
+        
+        // create reusable transporter object using SMTP transport
+        var transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: '...',
+                pass: '...'
+            }
+        });
+
+        // NB! No need to recreate the transporter object. You can use
+        // the same transporter object for all e-mails
+
+        // setup e-mail data with unicode symbols
+        var mailOptions = {
+            from: 'Teacup App <foo@blurdybloop.com>', // sender address
+            to: emailAddress, // list of receivers
+            subject: 'Hello', // Subject line
+            text: 'Hello world', // plaintext body
+            html: '<b>Hello world</b>' // html body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Message sent: ' + info.response);
+            }
+        });
+    }
 
 /**
  * Create user
  */
-exports.create = function(req, res, next) {
+exports.create = function (req, res, next) {
+    
     var user = new User(req.body);
 
     user.provider = 'local';
@@ -49,7 +84,7 @@ exports.create = function(req, res, next) {
     // because we set our user.provider to local our models/user.js validation will always be true
     req.assert('email', 'You must enter a valid email address').isEmail();
     req.assert('password', 'Password must be between 8-20 characters long').len(8, 20);
-    req.assert('username', 'Username cannot be more than 20 characters').len(1,20);
+    req.assert('username', 'Username cannot be more than 20 characters').len(1, 20);
     req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
 
     var errors = req.validationErrors();
@@ -59,20 +94,23 @@ exports.create = function(req, res, next) {
 
     // Hard coded for now. Will address this with the user permissions system in v0.3.5
     user.roles = ['authenticated'];
-    user.save(function(err) {
+    user.save(function (err) {
         if (err) {
             switch (err.code) {
-                case 11000:
-                case 11001:
-                    res.status(400).send('Username already taken');
-                    break;
-                default:
-                    res.status(400).send('Please fill all the required fields');
+            case 11000:
+            case 11001:
+                res.status(400).send('Username already taken');
+                break;
+            default:
+                res.status(400).send('Please fill all the required fields');
             }
 
             return res.status(400);
         }
-        req.logIn(user, function(err) {
+        
+        sendMail(user.email)
+        
+        req.logIn(user, function (err) {
             if (err) return next(err);
             return res.redirect('/');
         });
@@ -82,19 +120,19 @@ exports.create = function(req, res, next) {
 /**
  * Send User
  */
-exports.me = function(req, res) {
+exports.me = function (req, res) {
     res.jsonp(req.user || null);
 };
 
 /**
  * Find user by id
  */
-exports.user = function(req, res, next, id) {
+exports.user = function (req, res, next, id) {
     User
         .findOne({
             _id: id
         })
-        .exec(function(err, user) {
+        .exec(function (err, user) {
             if (err) return next(err);
             if (!user) return next(new Error('Failed to load User ' + id));
             req.profile = user;
